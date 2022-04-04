40421a5fc2545618a5bbbfcbbf4ce5c9de155082
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-04-04 14:12:37 +0530

    python script to identify the latest files in a s3 bucket/folder.

--- lastmodified.py
+++ lastmodified.py
@@ -1,6 +1,6 @@
 import boto3
 
-s3 = boto3.client('s3')
+
 bucket_one_conn=boto3.client('s3',
                      aws_access_key_id = "*****",aws_secret_access_key = "*****",
                      region_name="ap-south-1")
